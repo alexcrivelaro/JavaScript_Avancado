e11229a72ac6a7068ccc7079ae215dbc8461dfd6
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-16 16:11:16 -0200

    Uso de propriedades.

---
+++ JS-03-Propriedades.js
@@ -0,0 +1,15 @@
+function Animal() {
+    this.raca = "Cao";
+    this.nome = "Lulu";
+    this.idade = "20";
+    this.peso = "10";
+}
+
+var lulu = new Animal();
+lulu.raca = "Gato";
+lulu.nome = "Lulu";
+
+var xuxu = new Animal();
+xuxu.raca = "Cao";
+xuxu.nome = "Xuxu";
+
